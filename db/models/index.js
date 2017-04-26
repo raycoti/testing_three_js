63d2323b54df4fd55ef4b23140ad8a41be3f867b
ray coti <Raymond.coti@gmail.com>
2017-04-26 15:17:28 -0500

    initializes web page

--- db/models/index.js
+++ db/models/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// Require all the models
-	// Running each model (i.e. table) module (i.e. file) registers each model into Sequelize so any other part of the application could call Sequelize.model('user') OR Sequelize.models.user to get access to the `user` model.
-	// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
 
 const User = require('./user')
 
